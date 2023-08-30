e1f442a0430821602a7ad4e62555b50485e209dc
Samuel Silva <samuel7silva@outlook.com>
2023-08-30 10:57:40 -0300

    ajusta o botao para receber props como parâmetro

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,14 @@
-import { ReactNode } from 'react'
+import { ButtonHTMLAttributes, ReactNode } from 'react'
 
-function Button({ children }: { children: ReactNode }) {
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: ReactNode
+}
+
+function Button({ children, ...props }: ButtonProps) {
   return (
     <button
-      type="button"
-      className="bg-secondary h-12 w-56 rounded-xl text-xl font-semibold transition-all hover:opacity-80 md:h-16 md:w-80 md:text-2xl lg:h-20 lg:w-[464px] lg:text-4xl"
+      {...props}
+      className="h-12 w-56 rounded-xl bg-secondary text-xl font-semibold transition-all hover:opacity-80 md:h-16 md:w-80 md:text-2xl lg:h-20 lg:w-[464px] lg:text-4xl"
     >
       {children}
     </button>
