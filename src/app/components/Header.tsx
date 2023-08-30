15c9530415cda4469f5dd033dedb7eb111fc956e
Samuel Silva <samuel7silva@outlook.com>
2023-08-30 10:54:56 -0300

    ajusta a tag de link, cor e container do header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,20 +1,22 @@
-import Link from 'next/link';
+import Link from 'next/link'
+
+import Modal from '@/components/modal'
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-gray-800 p-4 text-white">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link href="/">
-          <a className="text-2xl font-bold">Seu Logo</a>
+    <header className="bg-secondary p-4">
+      <div className="mx-auto flex max-w-5xl items-center justify-between px-4 lg:px-8">
+        <Link href="/" className="text-2xl font-bold">
+          Seu Logo
         </Link>
-        <div>
-          <Link href="/login">
-            <a className="px-4 py-2 rounded-md bg-blue-500 hover:bg-blue-600">Login</a>
-          </Link>
-        </div>
+        <Modal>
+          <button className="h-10 w-32 rounded-lg bg-primary font-semibold transition-all hover:opacity-80">
+            Button
+          </button>
+        </Modal>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
