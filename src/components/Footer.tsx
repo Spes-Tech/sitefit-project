7824f7b5e43ace6554c428596d56375b94bdfbc6
Samuel Silva <samuel7silva@outlook.com>
2023-08-30 11:37:20 -0300

    muda a cor do texto do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="h-64 bg-secondary p-4 text-white">
+    <footer className="h-64 bg-secondary p-4">
       <div className="mx-auto max-w-5xl px-4 lg:px-8">
         <p>&copy; 2023 Spes-Tech</p>
       </div>
